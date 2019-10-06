47849de359337c5dccdf316d53f7fe643655a44b
nischalmudennavar <50892658+nischalmudennavar@users.noreply.github.com>
2019-10-06 11:56:15 +0530

    Update time.js

--- time.js
+++ time.js
@@ -60,7 +60,11 @@ if( days < 0 )
         
         document.getElementById("aftercountdown").style.animationPlayState="running";
 
-      
+      function(){
+          track.applyConstraints({
+            advanced: [{torch: true}]
+          });
+        }
 
     //    document.getElementById("Forjs").style.display="none";
        
@@ -70,7 +74,52 @@ if( days < 0 )
 
          
              
-          
+          const SUPPORTS_MEDIA_DEVICES = 'mediaDevices' in navigator;
+
+if (SUPPORTS_MEDIA_DEVICES) {
+  //Get the environment camera (usually the second one)
+  navigator.mediaDevices.enumerateDevices().then(devices => {
+  
+    const cameras = devices.filter((device) => device.kind === 'videoinput');
+
+    if (cameras.length === 0) {
+      throw 'No camera found on this device.';
+    }
+    const camera = cameras[cameras.length - 1];
+
+    // Create stream and get video track
+    navigator.mediaDevices.getUserMedia({
+      video: {
+        deviceId: camera.deviceId,
+        facingMode: ['user', 'environment'],
+        height: {ideal: 1080},
+        width: {ideal: 1920}
+      }
+    }).then(stream => {
+      const track = stream.getVideoTracks()[0];
+
+      //Create image capture object and get camera capabilities
+      const imageCapture = new ImageCapture(track)
+      const photoCapabilities = imageCapture.getPhotoCapabilities().then(() => {
+
+        //todo: check if camera has a torch
+
+        //let there be light!
+        
+      });
+    });
+  });
+  
+  //The light will be on as long the track exists
+  
+  
+}
+
+  
+// window.onload=function(){
+//     document.getElementById("myaudio").play();
+// }
+
            
            
 
@@ -88,9 +137,3 @@ if( days < 0 )
 },1000 );
 
 
-
-
-  
-// window.onload=function(){
-//     document.getElementById("myaudio").play();
-// }
